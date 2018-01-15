582224ef1eb561205dae9d36a6105e5f7c5300af
astropugay <astro.pugay@gmail.com>
2018-01-15 15:30:18 +0800

    todolist

---
+++ delete.php
@@ -0,0 +1,17 @@
+<?php 
+$id = $_POST['id'];
+
+$todos = file_get_contents('assets/todos.json');
+$todos = json_decode($todos, true);
+
+//delete task from the array $todos
+array_splice($todos, $id, 1);
+var_dump($todos);
+
+//update json file
+$file = fopen('assets/todos.json', 'w');
+fwrite($file, json_encode($todos, JSON_PRETTY_PRINT));
+fclose($file);
+// echo $id;
+
+ ?>
